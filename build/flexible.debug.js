a64d42d325180c47a30d093e9f3c9434e4533591
moocss <moocss@gmail.com>
2017-05-25 09:38:00 +0800

    修改设备判断

--- build/flexible.debug.js
+++ build/flexible.debug.js
@@ -3,8 +3,7 @@
     var docEl = doc.documentElement;
     var metaEl = doc.querySelector('meta[name="viewport"]');
     var flexibleEl = doc.querySelector('meta[name="flexible"]');
-    var dpr = 0;
-    var scale = 0;
+    var dpr = win.devicePixelRatio || 1;
     var tid;
     var flexible = lib.flexible || (lib.flexible = {});
     
@@ -31,33 +30,26 @@
         }
     }
 
-    if (!dpr && !scale) {
-        var isAndroid = win.navigator.appVersion.match(/android/gi);
-        var isChrome = win.chrome;
-        var isIPhone = win.navigator.appVersion.match(/iphone/gi);
-        var devicePixelRatio = win.devicePixelRatio;
-        var isX5 = /TBS\/\d+/.test(win.navigator.userAgent)
-        if (isIPhone || isChrome || isX5) {
-            // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
-                dpr = 3;
-            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
-                dpr = 2;
-            } else {
-                dpr = 1;
-            }
-        } else {
-            // 其他设备下，仍旧使用1倍的方案
-            dpr = 1;
-        }
-        scale = 1 / dpr;
+    var ua = win.navigator.userAgent;
+    var isAndroid = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
+    var isChrome = win.chrome;
+    var isIos = win.navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
+    var isX5 = /TBS\/\d+/.test(win.navigator.userAgent);
+    var UCversion = ua.match(/U3\/((\d+|\.){5,})/i);
+    var isUCHd = UCversion && parseInt(UCversion[1].split('.').join(''), 10) >= 80;
+    if (!isIos && !(isAndroid && isAndroid[1] > 534) && !isChrome && !isUCHd && !isX5) {
+        // 如果非iOS, 非Android4.3以上, 非UC内核等, 就不执行高清, dpr设为1;
+        dpr = 1;
     }
-
+    var scale = 1 / dpr;
+    
+    // 设置data-dpr属性，留作的css hack之用
     docEl.setAttribute('data-dpr', dpr);
     if (!metaEl) {
         metaEl = doc.createElement('meta');
         metaEl.setAttribute('name', 'viewport');
-        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+        // 最好设置一下width=device-width，在安卓中X5浏览器有bug
+        metaEl.setAttribute('content', 'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no');
         if (docEl.firstElementChild) {
             docEl.firstElementChild.appendChild(metaEl);
         } else {
@@ -69,8 +61,8 @@
 
     function refreshRem(){
         var width = docEl.getBoundingClientRect().width;
-        if (width / dpr > 540) {
-            width = 540 * dpr;
+        if (width / dpr > 640) {
+            width = 640 * dpr;
         }
         var rem = width / 10;
         docEl.style.fontSize = rem + 'px';
@@ -96,7 +88,6 @@
         }, false);
     }
     
-
     refreshRem();
 
     flexible.dpr = win.dpr = dpr;
